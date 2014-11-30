db7dd1b95c34e30f778963150e8f06f2da793470
richard.turton <richard.turton@mubaloo.com>
2014-11-30 16:43:57 +0000

    Added comment to highlight autolayout code

--- 1-Starter/Seuss/ActivityIndicator.swift
+++ 1-Starter/Seuss/ActivityIndicator.swift
@@ -12,15 +12,18 @@ extension UIViewController {
   
   func displayActivity(seconds: Int, completion:(()->Void)?) {
     let indicator = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
-    indicator.setTranslatesAutoresizingMaskIntoConstraints(false)
     indicator.layer.cornerRadius = 10.0
     indicator.backgroundColor = UIColor(white: 0.0, alpha: 0.4)
     view.addSubview(indicator)
+    
+    // Size and position using autolayout
+    indicator.setTranslatesAutoresizingMaskIntoConstraints(false)
     let views = ["indicator":indicator]
     view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("[indicator(150)]", options: nil, metrics: nil, views: views))
     view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[indicator(150)]", options: nil, metrics: nil, views: views))
     view.addConstraint(NSLayoutConstraint(item: indicator, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1, constant: 0))
     view.addConstraint(NSLayoutConstraint(item: indicator, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: 0))
+    
     
     view.tintAdjustmentMode = .Dimmed
     
