c2fde0d371d2f7cba469eaf3b0ca67ad5c30db8c
richard.turton <richard.turton@mubaloo.com>
2015-01-16 07:55:43 +0000

    Challenge solution

--- 4-Challenge/Seuss/BorderViews.swift
+++ 4-Challenge/Seuss/BorderViews.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-private let colorBarTag = 98
-private let backgroundTag = 99
+private var colorBarKey : Void?
+private var backgroundKey : Void?
 
 extension UIView {
     
@@ -18,20 +18,20 @@ extension UIView {
             if self.backgroundBorder == nil {
                 let backgroundBorder = UIView(frame: self.bounds)
                 backgroundBorder.autoresizingMask = .FlexibleHeight | .FlexibleWidth
-                backgroundBorder.tag = backgroundTag
                 backgroundBorder.backgroundColor = UIColor.whiteColor()
                 backgroundBorder.layer.borderWidth = 1.0;
                 backgroundBorder.layer.borderColor = UIColor.lightGrayColor().CGColor
                 self.insertSubview(backgroundBorder, atIndex:0)
+                self.backgroundBorder = backgroundBorder
             }
             if let colorBar = self.colorBar {
                 colorBar.backgroundColor = newValue
             } else {
                 let colorBar = UIView(frame: CGRect(x: 0.0, y: 0.0, width: self.bounds.width, height: 4.0))
                 colorBar.autoresizingMask = .FlexibleBottomMargin | .FlexibleWidth
-                colorBar.tag = colorBarTag
                 colorBar.backgroundColor = newValue
                 self.insertSubview(colorBar, aboveSubview: self.backgroundBorder!)
+                self.colorBar = colorBar
             }
         }
         get { return UIColor() }
@@ -39,13 +39,20 @@ extension UIView {
     
     private var backgroundBorder : UIView? {
         get {
-            return self.viewWithTag(backgroundTag)
+            return objc_getAssociatedObject(self, &backgroundKey) as? UIView
         }
+        set {
+            objc_setAssociatedObject(self, &backgroundKey, newValue, UInt(OBJC_ASSOCIATION_ASSIGN))
+        }
+        
     }
     
     private var colorBar : UIView? {
         get {
-            return self.viewWithTag(colorBarTag)
+            return objc_getAssociatedObject(self, &colorBarKey) as? UIView
+        }
+        set {
+            objc_setAssociatedObject(self, &colorBarKey, newValue, UInt(OBJC_ASSOCIATION_ASSIGN))
         }
     }
 }
